ddff83f452cd1db35d5740b0aa6c62fb24980704
LexDX <42230789+LexDX@users.noreply.github.com>
2021-02-11 20:36:23 +0300

    fix imports

--- Angular/src/app/app.module.ts
+++ Angular/src/app/app.module.ts
@@ -2,7 +2,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { DxDropDownBoxModule, DxDataGridModule } from 'devextreme-angular';
+import { DxDropDownBoxModule } from 'devextreme-angular/ui/drop-down-box';
+import { DxDataGridModule } from 'devextreme-angular/ui/data-grid';
 
 
 @NgModule({
