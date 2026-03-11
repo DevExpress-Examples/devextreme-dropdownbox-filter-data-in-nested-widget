be7d2fc0b3166131e1b8b42a5efddaa1e9d4919a
Diana Gromova <diana.gromova@devexpress.com>
2026-03-11 22:50:53 +0400

    Update app.component.ts

--- Angular/src/app/app.component.ts
+++ Angular/src/app/app.component.ts
@@ -22,6 +22,7 @@ interface OrderItem {
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
+  standalone: false,
 })
 export class AppComponent implements AfterViewInit {
   @ViewChild('dropDownBox', { static: false }) dropDownBox!: DxDropDownBoxComponent;
