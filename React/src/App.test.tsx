34264af66ce70af97ac53b9cd92d242970cade21
AlisherAmonulloev <AlisherAmonulloev@users.noreply.github.com>
2026-05-27 14:05:07 +0300

    Migrate React: update DevExtreme, flat ESLint config

--- React/src/App.test.tsx
+++ React/src/App.test.tsx
@@ -1,5 +1,5 @@
 import { render, screen } from '@testing-library/react';
-import App from './App';
+import App from './App.tsx';
 
 test('DropDownBox with search and embedded DataGrid', () => {
   render(<App />);
